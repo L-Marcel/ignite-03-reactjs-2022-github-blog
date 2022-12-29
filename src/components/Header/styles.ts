49703b5f0e575c20e064047173d94acd5a1b1d61
l-marcel <lmgh1312@gmail.com>
2022-12-29 08:53:00 -0300

    style: updating responsive mode

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -4,6 +4,8 @@ export const HeaderContainer = tw.header`
   flex
   justify-center
   items-start
+  object-contain
+  bg-no-repeat
   w-full
   h-[18.5rem]
 `;
--- src/styles/pages/issueStyles.ts
+++ src/styles/pages/issueStyles.ts
@@ -3,9 +3,9 @@ import tw from "tw-tailwind";
 export const IssuePageContainer = tw.main`
   flex
   flex-col
-  px-11
-  md:px-36
-  lg:px-56
+  px-8
+  md:px-10
+  lg:px-36
   lg2:px-60
   w-full
 `;
